f1199c7a365610afbc8077d507fd7f6ce8c0cd01
CodeKiro <kirofiniello@outlook.it>
2024-06-12 00:43:14 +0200

    added jumbotron, and a big box for the cards

--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -9,11 +9,34 @@ export default {
 </script>
 
 <template>
-    <h1>
-        Main 
-    </h1>
+    <div class="jumbotron">
+    </div>
+    <button>CURRENT SERIES</button>
+    <div class="cards">
+
+    </div>
 </template>
 
-<style scoped>
-
+<style lang="scss" scoped>
+@use '../styles/general.scss' as *;
+@use '../styles/partials/variables.scss' as *;
+.jumbotron{
+    width: 100%;
+    height: 450px;
+    background-image: url(../assets/img/jumbotron.jpg);
+}
+button{
+    background-color: $active_color;
+    color: white;
+    padding: 1rem;
+    font-size: 1.5rem;
+    font-weight: bold;
+    position: absolute;
+    left: 20%;
+    top: 45%;
+}
+.cards{
+    background-color: $bg_main_color;
+    width: 100%;
+}
 </style>
